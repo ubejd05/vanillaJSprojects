00aa80e8322c8746213926825c7c258d7bc9792a
Ubejd <ubejd.haziri2005@gmail.com>
2021-09-28 22:10:28 +0200

    New LyricsSearch project

---
+++ 16.LyricsSearch/app.js
@@ -0,0 +1,87 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+
+
+const apiURL = 'https://api.lyrics.ovh';
+
+
+// Search by song or artist 
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`)
+  const data = await res.json();
+  console.log(data);
+  
+  showData(data)
+}
+
+// Show song and artist in DOM
+function showData(data) {
+  // let output = "";
+  // data.data.forEach((song) => {
+  //   output += `
+  //   <li>
+  //     <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //     <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"
+  //     >Get Lyrics</button>
+  //   </li>
+  //   `;
+  // })
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  // `;
+
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data.map((song) =>
+        `<li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"
+          >Get Lyrics</button>
+        </li>`
+      ).join('')}
+    </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+      ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+    `; 
+  } else 
+  more.innerHTML = '';
+}
+
+async function getMoreSongs(url) {
+  let x = `https://${url.slice(7)}`;
+  console.log(`${x}`);
+  const res = await fetch(x, {headers:{
+    'Access-Control-Allow-Origin': `https://api.deezer.com/search?limit=15&q=one&index=15`
+  }});
+  const data = await res.json();
+  console.log(data);
+  
+  // showData(data)
+}
+
+
+// Event listeners
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim()
+  
+  if (!searchTerm) {
+    alert('Please type in a search term')
+  } else {
+    searchSongs(searchTerm)
+  }
+})
+
+
+// http://api.deezer.com/search?limit=15&q=one&index=15
